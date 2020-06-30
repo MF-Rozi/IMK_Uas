139673cfa9546e465c5dc6dc2f7918ea844af40d
mf-rozi <m.fakhrulrozi10@gmail.com>
2020-06-30 21:02:19 +0700

    Edit Login Page

--- login_page.php
+++ login_page.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="/css/login.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Login Page!</title>
 </head>
 <body>
   <!-- Login Menu -->
